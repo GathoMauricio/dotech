62eea96c7f4d6c1e672aad30523ddda914de350e
Oscar Mauricio Juarez Soriano <mauricio2769@gmail.com>
2020-12-18 17:09:48 -0600

    whitdrawal_departments migration,model and controller

---
+++ app/WhitdrawalDepartment.php
@@ -0,0 +1,13 @@
+<?php
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+class WhitdrawalDepartment extends Model
+{
+    protected $table = 'whitdrawal_departments';
+	protected $primaryKey = 'id';
+	public $timestamps = true;
+    protected $fillable = [
+        'id',
+        'name'
+    ];
+}
