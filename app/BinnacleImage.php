8e0145cb681877adb3635023093e2da0b7109694
Oscar Mauricio Juarez Soriano <mauricio2769@gmail.com>
2021-01-08 14:47:25 -0600

    make binnacke and binnacle_image migration, model and controls

---
+++ app/BinnacleImage.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Auth;
+
+class BinnacleImage extends Model
+{
+    protected $table = 'binnacle_images';
+    protected $primaryKey = 'id';
+    public $timestamps = true;
+    protected $fillable = [
+        'id',
+        'binnacle_id',
+        'author_id',
+        'description',
+        'created_at',
+        'updated_at'
+    ];
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($query) {
+            $query->author_id = Auth::user()->id;
+        });
+    }
+    public function binnacle()
+    {
+        return $this->belongsTo(
+                'App\Binnacle',
+                'binnacle_id',
+                'id'
+            )
+            ->withDefault();
+    }
+    public function author()
+    {
+        return $this->belongsTo(
+                'App\User',
+                'author_id',
+                'id'
+            )
+            ->withDefault();
+    }
+}
