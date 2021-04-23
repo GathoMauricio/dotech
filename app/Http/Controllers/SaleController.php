3e46d43d6673212a055d34d7288534e693b69595
Mauricio Soriano <mauricio2769@gmail.com>
2021-04-23 15:11:33 -0500

    hot fix

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -91,8 +91,8 @@ class SaleController extends Controller
             $totalRetiros += floatval($whitdrawal->quantity);
         }
 
-        $costoProyecto = number_format($sale->estimated + ($sale->estimated * 0.16),2);
-        $utilidad = number_format($costoProyecto - $totalRetiros,2);
+        $costoProyecto = $sale->estimated + ($sale->estimated * 0.16);
+        $utilidad = $costoProyecto - $totalRetiros;
         $comision = number_format((($utilidad / 1.16) * $sale->commision_percent) / 100 ,2);
 
 
