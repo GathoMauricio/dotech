fc804fc1773e2ad02f745ff880e4800df9dfb9f6
Mauricio Soriano <mauricio2769@gmail.com>
2021-06-22 15:23:17 -0500

    view users on gerente perfil

--- resources/views/config/menu.blade.php
+++ resources/views/config/menu.blade.php
@@ -3,7 +3,7 @@
     &nbsp;&nbsp;
     <a href="{{ route('provider_index') }}"><span class="icon-cart"></span> Proveedores de retiro</a>
     &nbsp;&nbsp;
-    @if(Auth::user()->rol_user_id == 1)
+    @if(Auth::user()->rol_user_id == 1 || Auth::user()->rol_user_id == 2)
     <a href="{{ route('index_department') }}"><span class="icon-tree"></span> Departamentos de retiro</a>
     &nbsp;&nbsp;
     <a href="{{ route('index_account') }}"><span class="icon-credit-card"></span> Cuentas de retiro</a>
