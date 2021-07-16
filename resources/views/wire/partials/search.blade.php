7bd5682056933a9c9343ccfb87a12f581e2478d6
Mauricio Soriano <mauricio2769@gmail.com>
2021-07-15 20:06:30 -0500

    mostrar elementos livewire

--- resources/views/wire/partials/search.blade.php
+++ resources/views/wire/partials/search.blade.php
@@ -1,3 +1,4 @@
 <input wire:model="search" class="form-control" placeholder="Buscar..." />
 <span id="span_result">@if(strlen($search) >0) Resultados de "{{ $search }}" @else &nbsp; @endif</span>
 <br/>
+<img src="{{ asset('img/wire.png') }}" width="120" class="float-right">
