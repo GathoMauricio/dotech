5bfbbe5d3b45cadb6950a5f739623f139aae8b36
Mauricio Soriano <mauricio2769@outlook.es>
2023-01-17 20:51:26 -0600

    desactivar upload dump db

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('dump:mysql')->everyFiveMinutes();
+        //$schedule->command('dump:mysql')->everyFiveMinutes();
     }
 
     /**
