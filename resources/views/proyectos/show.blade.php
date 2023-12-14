7bd172b9a78a77f6b9e8789289d52db345c9b26a
OMJS2769 <mauricio2769@outlook.es>
2023-12-14 00:09:41 -0600

    enviar bitacora

--- resources/views/proyectos/show.blade.php
+++ resources/views/proyectos/show.blade.php
@@ -457,7 +457,7 @@
             const route = $("#txt_get_binnacle").val();
             $.ajax({
                 type: "GET",
-                url: "/binnacle_show_json/" + binnacle_id,
+                url: "./binnacle_show_json/" + binnacle_id,
                 data: {},
                 success: (data) => {
                     console.log(data);
