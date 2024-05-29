73d5295ecff4f9cbe7d2285cb3d503dabb8362c2
OMJS2769 <mauricio2769@outlook.es>
2024-05-28 18:34:10 -0600

    hotfix

--- app/Console/Commands/DumpMysql.php
+++ app/Console/Commands/DumpMysql.php
@@ -41,7 +41,7 @@ class DumpMysql extends Command
             ->setDbName(\Config::get('app.bd_name'))->setUserName(\Config::get('app.bd_user'))
             ->setPassword(\Config::get('app.bd_password'))
             ->dumpToFile(\Config::get('app.app_route') . 'storage/dump_db/dump_' . \Config::get('app.bd_name') . '.sql');
-        \Log::info("Base de datos creada..." . date('Y-m-d'));
+        //\Log::info("Base de datos creada..." . date('Y-m-d'));
 
         // $disk = \Storage::disk('gcs');
         // $disk->put("DB_dotech.sql",\File::get(storage_path('dump_db/dump_'.date('Y-m-d').'.sql')));
