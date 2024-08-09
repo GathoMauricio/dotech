26255d7748f18f6a6b9cc202432b94351339071f
OMJS2769 <mauricio2769@outlook.es>
2024-08-09 00:46:17 -0600

    agregar campo source al modelo de salida de vehiculo

--- app/VehicleHistoryImage.php
+++ app/VehicleHistoryImage.php
@@ -7,8 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class VehicleHistoryImage extends Model
 {
     protected $table = 'vehicle_history_images';
-	protected $primaryKey = 'id';
-	public $timestamps = true;
+    protected $primaryKey = 'id';
+    public $timestamps = true;
 
     protected $fillable = [
         'id',
@@ -16,35 +16,34 @@ class VehicleHistoryImage extends Model
         'vehicle_history_id',
         'image',
         'description',
+        'sourse',
         'created_at',
         'updated_at'
     ];
     protected static function boot()
-	{
-		parent::boot();
+    {
+        parent::boot();
 
-		static::creating(function ($query) {
-			$query->author_id = \Auth::user()->id;
-		});
-	}   
+        static::creating(function ($query) {
+            $query->author_id = \Auth::user()->id;
+        });
+    }
     public function history()
     {
-        return $this->belongsTo
-        (
-            'App\vehicle_history_id',
-            'vehicle_id',
-            'id'
-        )
-        ->withDefault();
+        return $this->belongsTo(
+                'App\vehicle_history_id',
+                'vehicle_id',
+                'id'
+            )
+            ->withDefault();
     }
     public function author()
     {
-        return $this->belongsTo
-        (
-            'App\User',
-            'author_id',
-            'id'
-        )
-        ->withDefault();
+        return $this->belongsTo(
+                'App\User',
+                'author_id',
+                'id'
+            )
+            ->withDefault();
     }
 }
