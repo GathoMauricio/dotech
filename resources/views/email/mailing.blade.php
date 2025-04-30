66d13180c680c71a9c969e662dacfd23b815c9a7
OMJS2769 <mauricio2769@outlook.es>
2025-04-30 13:00:32 -0600

    refs mail

--- resources/views/email/mailing.blade.php
+++ resources/views/email/mailing.blade.php
@@ -1,1 +1,9 @@
 {!! $mailing->body !!}
+<br><br><br><br><br><br><br><br><br>
+<footer>
+    Dot Tecnología e Inovación | Laguna de San Cristóbal #99 Col. Anáhuac. Del Miguel Hidalgo. CDMX. 1132
+    <br>
+    Tel: 5614545274
+    <br>
+    <a href="mailto:http://www.dotredes.com">http://www.dotredes.com</a>
+</footer>
